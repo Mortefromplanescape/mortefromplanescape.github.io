039444830e3674c96b33c63c2ab64e744671b1de
Mortefromplanescape <boreich89@gmail.com>
2017-05-17 06:28:35 +0300

    17.05.17

---
+++ assets/templates/metal/js/main.js
@@ -0,0 +1,12 @@
+/*  $('.mbc-left > li > ul' )
+	.hide()
+	.click(function(event) {
+		event.stopPropagation();
+	});
+  
+	$('.mbc-left > li').toggle(function() {
+		$(this).find('ul').slideDown();
+  }, function() {
+  	$(this).find('ul').slideUp();
+  });
+});*/
